a17d0deb2881f684ac856665bd007ae664030038
Nicholas Glesmann <nicholasglesmann@gmail.com>
2023-04-18 16:42:45 -0700

    force login for non public pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,43 @@
 import Meta from "./Meta";
 import Nav from "./Nav";
 import styles from "../styles/Layout.module.css";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react";
+import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
+
+const publicRoutes = ["/", "/about"];
 
 export const Layout = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPath, setCurrentPath] = useState("");
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+    setIsLoading(false);
+  });
+
+  let Rendered;
+
+  if (isLoading) {
+    Rendered = Spinner;
+  } else if (!session && !publicRoutes.includes(currentPath)) {
+    Rendered = () => <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />;
+  } else {
+    Rendered = () => children;
+  }
+
   return (
     <>
       <Meta />
       <Nav />
       <div className={styles.container}>
-        <main className={styles.main}>{children}</main>
+        <main className={styles.main}>
+          <Rendered />
+        </main>
       </div>
     </>
   );
